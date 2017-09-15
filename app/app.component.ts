274abe29fce8943f80d6f14c482b8bd4f48f88a2
Granton Mchana <gmchana254@gmail.com>
2017-09-15 18:43:33 +0300

    chanbed some content

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,7 +9,9 @@ import { MealListComponent } from './meal-list.component';
     </div>
     <div class="container">
 
-
+<div class="container">
+<div class"row">
+<div class="col-md-8">
     <h3>Best Meals</h3>
         <div *ngFor="let currentMeal of favoriteMeals">
           <p>Name:{{meal.name}}</p>
@@ -30,33 +32,19 @@ import { MealListComponent } from './meal-list.component';
         </div>
         <button type="confirm!" class="btn btn-primary">Add</button>
         </div>
-        <div *ngFor="let currentTask of tasks">
-          <h3>{{ c }}</h3>
-          <button (click)="showDetails(currentTask)">Edit</button>
-        </div>
-        <div *ngIf="selectedTask">
-          <h1>Edit Task</h1>
-          <div>
-            <label>Enter Task Description:</label>
-            <input [(ngModel)]="selectedTask.description">
-          </div>
-          <div>
-            <label>Enter Task ID:</label>
-            <input [(ngModel)]="selectedTask.id">
-            <button (click)="finishedEditing()">Done</button>
-          </div>
-        </div>
 
+          </div>
+        </div>
   </div>
   `
 })
 
 export class AppComponent {
 meals:Meal[]=[
-      new Meal("Avocado; 160 calories per 100 grams.", 0),
-      new Meal("Nut Seed and Milk; 1000 calories per 100.", 1),
-      new Meal("Bryani; 168 calories er 100 grams.", 2),
-      new Meal("Dried Apples; 104 calories.", 3)
+      new Meal("Avocado", 160, "minssed with onions", 0),
+      new Meal("Nut Seed and Milk", 1000, "What a huge one yeah.", 1),
+      new Meal("Bryani", 168,"The lovly one.", 2),
+      new Meal("Dried Apples", 104, "Just an appetiser.", 3)
   ];
 
 }
